bc71da66c58f7a55545b70f9a26e21c6dc1d832f
Kunal Chauhan <52076199+kc8055@users.noreply.github.com>
2020-12-26 22:01:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,44 @@
-from flask import Flask, render_template, flash, request, url_for, redirect, session
 import numpy as np
 import pandas as pd
-import re
-import os
-import tensorflow as tf
-from numpy import array
-from keras.datasets import imdb
-from keras.preprocessing import sequence
-from keras.models import load_model
+from flask import Flask, request, render_template
+import pickle
 
-IMAGE_FOLDER = os.path.join('static', 'img_pool')
+app = Flask(__name__,template_folder="templates")
+model = pickle.load(open('breast_cancer_detector.pickle', 'rb'))
 
-app = Flask(__name__,template_folder='templates')
-
-app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
-
-def init():
-    global model
-    # load the pre-trained Keras model
-    model = load_model('sentiment_analysis.h5')
-
-#########################Code for Sentiment Analysis
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/',methods=["GET",'POST'])
 def home():
+    return render_template('home.html')
 
-    return render_template("home.html")
-
-@app.route('/sentiment_analysis_prediction', methods = ['POST', "GET"])
-def sent_anly_pred():
-    if request.method=='POST':
-        text = request.form['COMMENT']
-        max_review_length = 500
-        word_to_id = imdb.get_word_index()
-        strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
-        text = text.lower().replace("<br />", " ")
-        text=re.sub(strip_special_chars, "", text.lower())
-
-        words = text.split() #split string into a list
-        x_test = [[word_to_id[word] if (word in word_to_id and word_to_id[word]<=20000) else 0 for word in words]]
-        x_test = sequence.pad_sequences(x_test, maxlen=500) # Should be same which you used for training data
-        vector = np.array([x_test.flatten()])
+@app.route('/predict',methods=['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+       'mean smoothness', 'mean compactness', 'mean concavity',
+       'mean concave points', 'mean symmetry', 'mean fractal dimension',
+       'radius error', 'texture error', 'perimeter error', 'area error',
+       'smoothness error', 'compactness error', 'concavity error',
+       'concave points error', 'symmetry error', 'fractal dimension error',
+       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+       'worst smoothness', 'worst compactness', 'worst concavity',
+       'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    output = model.predict(df)
         
-        probability = model.predict(array([vector][0]))[0][0]
-        class1 = model.predict_classes(array([vector][0]))[0][0]
-        if class1 == 0:
-            sentiment = 'Negative'
-            img_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Sad_Emoji.png')
-        else:
-            sentiment = 'Positive'
-            img_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'Smiling_Emoji.png')
-    return render_template('<h1>sahil<h1/>')
+    if output == 0:
+        res_val = "** Breast Cancer **"
+    else:
+        res_val = "no Breast Cancer"
+        
 
-@app.route('/result',methods=['GET','POST'])
+    return render_template('result.html', prediction_text='Patient has {}'.format(res_val))
+
+@app.route('/result',methods=["GET",'POST'])
 def result():
     return render_template('result.html')
-#########################Code for Sentiment Analysis
 
 if __name__ == "__main__":
-    init()
     app.run()
